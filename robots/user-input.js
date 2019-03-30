0a0e50a2b1d6fcd5418ca93536db3b5080c39bd5
vsalbuq <vinicius.albuquerque@outlook.com>
2019-03-30 10:10:43 -0300

    feat: add state robot

--- robots/user-input.js
+++ robots/user-input.js
@@ -1,8 +1,15 @@
 const readline = require('readline-sync')
+const state = require('./state')
 
-function robot (content) {
+function robot () {
+    const content = {
+        maximumSentences: 7
+    }
+    
     content.searchTerm = askAndReturnSearchTerm()
     content.prefix = askAndReturnPrefix()
+
+    state.save(content)
 
     function askAndReturnSearchTerm() {
         return readline.question('Type a Wikipedia search term: ')
